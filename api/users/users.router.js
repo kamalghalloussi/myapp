b6be5bcf8207b25c0f7eb1e34ba67ec11dd36e62
kamalghalloussi <contact@oratos-web.fr>
2023-12-10 11:08:31 +0100

    Signed-off-by: kamalghalloussi <contact@oratos-web.fr>

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -3,7 +3,6 @@ const usersController = require("./users.controller");
 const authMiddleware = require("./middlewares/auth"); 
 const router = express.Router();
 
-// Utiliser le middleware d'authentification pour protéger les routes si nécessaire
 router.use("/:id", authMiddleware.verifyUser); 
 
 router.get("/", usersController.getAll);
